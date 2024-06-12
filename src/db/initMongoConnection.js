eaae5be04013d3e82603c855f52f0033f9fc1ca7
Marensfc <alexeymaren12@gmail.com>
2024-06-12 13:55:04 +0300

    Create initMongoDb and collection

---
+++ src/db/initMongoConnection.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+import { env } from '../utils/env.js';
+import { MONGODB_CONSTANTS } from '../constants/mongoDB-constants.js';
+
+export const initMongoConnection = async () => {
+  const user = env(MONGODB_CONSTANTS.MONGODB_USER);
+  const password = env(MONGODB_CONSTANTS.MONGODB_PASSWORD);
+  const url = env(MONGODB_CONSTANTS.MONGODB_URL);
+
+  try {
+    mongoose.connect(
+      `mongodb+srv://${user}:${password}@${url}/?retryWrites=true&w=majority`,
+    );
+    console.log('Mongo connection successfully established!');
+  } catch (error) {
+    console.log('Error while setting up mongo connection', error);
+    throw error;
+  }
+};
--- src/server.js
+++ src/server.js
@@ -1,10 +1,28 @@
 import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
 import { env } from './utils/env.js';
 
 const PORT = env('PORT');
 
 export const setupServer = () => {
   const app = express();
+
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
 
   app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 };
