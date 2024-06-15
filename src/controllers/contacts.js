6eb30350f2624d9b76dedd16f495ebf622720ad0
Marensfc <alexeymaren12@gmail.com>
2024-06-15 11:45:13 +0300

    Update Error handler, organized file's structure

---
+++ src/controllers/contacts.js
@@ -0,0 +1,28 @@
+import { createError } from 'http-errors';
+import { getAllContacts, getContactById } from '../services/contacts.js';
+
+export const getAllContactsController = async (req, res) => {
+  const contacts = await getAllContacts();
+
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully found contacts!',
+    data: contacts,
+  });
+};
+
+export const getContactByIdController = async (req, res, next) => {
+  const { contactId } = req.params;
+  const contact = await getContactById(contactId);
+
+  if (!contact) {
+    next(createError(404, 'Contact not found'));
+    return;
+  }
+
+  res.status(200).json({
+    status: 200,
+    message: `Successfully found contact with id ${contactId}!`,
+    data: contact,
+  });
+};
---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,16 @@
+import { isHttpError } from 'http-errors';
+
+export const errorHandler = async (err, req, res, next) => {
+  if (isHttpError(err)) {
+    res.status(err.status).json({
+      status: err.status,
+      message: err.message,
+    });
+  }
+
+  res.status(500).json({
+    status: 500,
+    message: 'Something went wrong',
+    data: err.message,
+  });
+};
