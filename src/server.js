57e1aa51ff0b591b1e5267b0231a2402dc36b076
Marensfc <alexeymaren12@gmail.com>
2024-06-23 14:25:10 +0300

    change Server.js

--- src/server.js
+++ src/server.js
@@ -19,13 +19,13 @@ export const setupServer = () => {
 
   app.use(cors());
 
-  // app.use(
-  //   pino({
-  //     transport: {
-  //       target: 'pino-pretty',
-  //     },
-  //   }),
-  // );
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
 
   app.use(contactsRouter);
 
